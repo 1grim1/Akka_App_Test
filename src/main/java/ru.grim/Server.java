4f3be1e762246392865215dd0e12b0ff3acc4d7e
grim <dima.ggrimbergg@gmail.com>
2020-12-16 02:28:27 +0300

    Scripted auto-commit on change (2020-12-16 02:28:27) by gitwatch.sh

--- src/main/java/ru.grim/Server.java
+++ src/main/java/ru.grim/Server.java
@@ -6,6 +6,7 @@ import akka.actor.Props;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.server.Route;
 import akka.pattern.PatternsCS;
+import akka.routing.RoundRobinPool;
 
 import java.util.regex.Pattern;
 import java.util.concurrent.CompletionStage;
@@ -13,14 +14,19 @@ import java.util.concurrent.CompletionStage;
 public class Server {
 
     private ActorRef storeActor;
+    private ActorRef actorPackageTest;
+    private ActorRef performerActor;
 
     public static final String SERVER_NAME = "localhost";
     public static final int PORT = 2077;
-
+    public static final int POOL_NUM = 6;
+    public static final String ACTOR_PACKAGE_TEST = "actorPackageTest";
+    public static final String ACTOR_PERFORMER = "actorPerformer";
 
     private Server(final ActorSystem actorSystem){
         storeActor = actorSystem.actorOf(Props.create(StoreActor.class));
-
+        actorPackageTest = actorSystem.actorOf(Props.create(ActorPackageTest.class), ACTOR_PACKAGE_TEST);
+        performerActor =  actorSystem.actorOf(new RoundRobinPool(POOL_NUM).props(Props.create(ActorPerformer.class), ACTOR_PERFORMER));
     }
 
     public static void main(String[] args){
@@ -29,13 +35,7 @@ public class Server {
     }
 
     private Route createRoute(){
-        return route(
-                get(() -> param("packageID", (packageID) -> {
-                    CompletionStage<> result = PatternsCS.ask(
-
-                    )
-                }))
-        )
+        return;
     }
 
 }

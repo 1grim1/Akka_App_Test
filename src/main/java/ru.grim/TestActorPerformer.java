e1e1cf6777300e15da0b3aba7685330fa00e14ff
grim <dima.ggrimbergg@gmail.com>
2020-12-17 02:55:36 +0300

    Scripted auto-commit on change (2020-12-17 02:55:36) by gitwatch.sh

--- src/main/java/ru.grim/TestActorPerformer.java
+++ src/main/java/ru.grim/TestActorPerformer.java
@@ -1,10 +1,19 @@
 package ru.grim;
 
 import akka.actor.AbstractActor;
+import akka.actor.ActorSelection;
+import akka.japi.pf.ReceiveBuilder;
 
 public class TestActorPerformer extends AbstractActor {
+    private static final String SCRIPT_NAME = "/user/storeActor";
+    private ActorSelection actorSelection = getContext().actorSelection(SCRIPT_NAME);
+
     @Override
     public Receive createReceive() {
-        return null;
+        return ReceiveBuilder.create()
+                .match(TestMessage.class, testMessage -> {
+                    actorSelection.tell(new StoreMessage());
+                })
+                .build();
     }
 }

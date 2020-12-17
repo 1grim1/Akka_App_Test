14692c33cabc645840ddf193c82d2cb32e03ae20
grim <dima.ggrimbergg@gmail.com>
2020-12-17 03:18:02 +0300

    Scripted auto-commit on change (2020-12-17 03:18:02) by gitwatch.sh

--- src/main/java/ru.grim/ActorPackageTest.java
+++ src/main/java/ru.grim/ActorPackageTest.java
@@ -1,11 +1,28 @@
 package ru.grim;
 
 import akka.actor.AbstractActor;
+import akka.actor.ActorSelection;
+import akka.japi.pf.ReceiveBuilder;
 
 public class ActorPackageTest extends AbstractActor {
+    private final String ACTOR_PERFORMER_PATH = "/user/testActorPerformer";
+
+    private ActorSelection actorSelection = getContext().actorSelection(ACTOR_PERFORMER_PATH);
 
     @Override
     public Receive createReceive() {
-        return null;
+        return ReceiveBuilder
+                .create()
+                .match(TestGroupMessage.class, testGroupMessage -> {
+                    for (Test test : testGroupMessage.getTestsList()){
+                        actorSelection.tell(new TestMessage(
+                                testGroupMessage.getPackageID(),
+                                testGroupMessage.getFunctionName(),
+                                testGroupMessage.getJsScript(),
+                                test),
+                                self());
+                    }
+                })
+                .build();
     }
 }
--- src/main/java/ru.grim/TestGroupMessage.java
+++ src/main/java/ru.grim/TestGroupMessage.java
@@ -1,2 +1,57 @@
-package ru.grim;public class TestGroupMessage {
+package ru.grim;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.ArrayList;
+
+public class TestGroupMessage {
+    public static final String PACKAGE_ID = "packageID";
+    public static final String FUNCTION_NAME = "functionName";
+    public static final String JS_SCRIPT = "jsScript";
+    public static final String TEST = "test";
+
+    @JsonProperty(PACKAGE_ID)
+    private int packageID;
+
+    @JsonProperty(FUNCTION_NAME)
+    private String  functionName;
+
+    @JsonProperty(JS_SCRIPT)
+    private String jsScript;
+
+    @JsonProperty(TEST)
+    private ArrayList<Test> testsList;
+
+    @JsonCreator
+    TestGroupMessage(
+            @JsonProperty(PACKAGE_ID) int packageID,
+            @JsonProperty(FUNCTION_NAME) String functionName,
+            @JsonProperty(JS_SCRIPT) String jsScript,
+            @JsonProperty(TEST) ArrayList<Test> testsList)
+    {
+        this.packageID = packageID;
+        this.testsList = testsList;
+        this.functionName = functionName;
+        this.jsScript = jsScript;
+    }
+
+    //getters
+
+    public int getPackageID(){
+        return this.packageID;
+    }
+
+    public String getFunctionName(){
+        return this.functionName;
+    }
+
+    public String getJsScript(){
+        return this.jsScript;
+    }
+
+    public ArrayList<Test> getTestsList(){
+        return this.testsList;
+    }
+
 }
